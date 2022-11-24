1bb1d882f7ff23318cf693a80c79309ca12e06b7
azealyx <95056079+azealyx@users.noreply.github.com>
2022-11-25 05:36:28 +0800

    Enable logging

--- Default.cmd
+++ Default.cmd
@@ -1,1 +1,4 @@
-@powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Win10.ps1" -include "%~dp0Win10.psm1" -preset "%~dpn0.preset"
+@echo off
+set timestamp=%DATE:/=-%_%TIME::=-%
+set timestamp=%timestamp: =%
+@powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Win10.ps1" -include "%~dp0Win10.psm1" -preset "%~dpn0.preset" -log alyx-log_%timestamp%.txt
