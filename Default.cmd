8bb2e863dc0e30e46042df2306c0ac4f83bdbe5a
azealyx <95056079+azealyx@users.noreply.github.com>
2023-04-27 01:10:45 +0800

    Preparations for Windows 11 support

--- Default.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-set timestamp=%DATE:/=-%_%TIME::=-%
-set timestamp=%timestamp: =%
-@powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Win10.ps1" -include "%~dp0Win10.psm1" -preset "%~dpn0.preset" -log alyx-log_%timestamp%.txt
